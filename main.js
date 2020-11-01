1d746c69fab569fc9969ce89604ae9cba3969864
Pablo <72697714+Pabloo22@users.noreply.github.com>
2020-11-01 19:33:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+mybutton = document.getElementById("myBtn");
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
